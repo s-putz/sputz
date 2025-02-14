7dd9cd91cc0df70c5027bc00bc38cdf592559915
s-putz <sputz@flashnet.at>
2025-02-14 18:37:31 +0100

    Fade In

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function visNav(){
-    var x = document.getElementById("navbar");
-    if (x.style.display === "none"|| x.style.display === ""){
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
-
